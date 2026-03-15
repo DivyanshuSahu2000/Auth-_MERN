f2fcf3dfeca572ec101330a506b4e93c8ae3501c
DivyanshuSahu2000 <sdivyanshusahu172@gmail.com>
2026-03-15 23:07:18 +0530

    frontend login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import { useState } from "react";
+import { TextField, Button, Container, Typography, Box } from "@mui/material";
+import { loginUser } from "../services/AuthServices";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await loginUser(form);
+
+      localStorage.setItem("token", data.token);
+
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+      alert(error.response?.data?.message || "Login failed");
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <Box mt={8}>
+        <Typography variant="h4" mb={3}>
+          Login
+        </Typography>
+
+        <form onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            label="Email"
+            name="email"
+            margin="normal"
+            onChange={handleChange}
+          />
+
+          <TextField
+            fullWidth
+            label="Password"
+            type="password"
+            name="password"
+            margin="normal"
+            onChange={handleChange}
+          />
+
+          <Button fullWidth variant="contained" type="submit" sx={{ mt: 2 }}>
+            Login
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
 };
 
 export default Login;
