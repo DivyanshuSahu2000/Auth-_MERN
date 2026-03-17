c867584f32ba5a3078d5f162a2ac69dbda97667a
DivyanshuSahu2000 <sdivyanshusahu172@gmail.com>
2026-03-17 23:37:02 +0530

    commit

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { getProfile } from "../services/AuthServices";
+import { useNavigate } from "react-router-dom";
+import { Container, Typography, Button, Box } from "@mui/material";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const token = localStorage.getItem("token");
+
+      if (!token) {
+        navigate("/login");
+        return;
+      }
+
+      try {
+        const data = await getProfile(token);
+        setUser(data);
+      } catch (error) {
+        console.log(error);
+        localStorage.removeItem("token");
+        navigate("/login");
+      }
+    };
+
+    fetchProfile();
+  }, [navigate]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
+  return (
+    <Container>
+      <Box mt={5}>
+        <Typography variant="h4">Dashboard</Typography>
+
+        {user && (
+          <>
+            <Typography mt={2}>Name: {user.name}</Typography>
+            <Typography>Email: {user.email}</Typography>
+          </>
+        )}
+
+        <Button
+          variant="contained"
+          color="error"
+          sx={{ mt: 3 }}
+          onClick={handleLogout}
+        >
+          Logout
+        </Button>
+      </Box>
+    </Container>
+  );
 };
 
 export default Dashboard;
--- frontend/src/services/AuthServices.jsx
+++ frontend/src/services/AuthServices.jsx
@@ -4,3 +4,12 @@ export const loginUser = async (data) => {
   const response = await axios.post(`${API}/login`, data);
   return response.data;
 };
+
+export const getProfile = async (token) => {
+  const response = await axios.get(`${API}/profile`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
