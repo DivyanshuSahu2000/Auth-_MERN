75fb2d10025c4afa95f789173dedd8c1e076c187
DivyanshuSahu2000 <sdivyanshusahu172@gmail.com>
2026-03-28 22:43:12 +0530

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://authflow-mern-frontend.vercel.app/",
+    credentials: true,
+  })
+);
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("connecteed");
