c5cd25b523ad5cbac4072aa1aec7a2c520ee8062
DivyanshuSahu2000 <sdivyanshusahu172@gmail.com>
2026-03-30 23:00:02 +0530

    deployment issue

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://authflow-mern-frontend.vercel.app/",
+    origin: "https://authflow-mern-frontend.vercel.app",
     credentials: true,
   })
 );
